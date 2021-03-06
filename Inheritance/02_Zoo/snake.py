dcadf4ce3b60dc73baa24ee01100d341caf5b429
Olga Yordanova <olga.yordanova.oy@gmail.com>
2021-03-06 06:56:26 +0200

    Add files via upload

---
+++ Inheritance/02_Zoo/snake.py
@@ -0,0 +1,4 @@
+from project.reptile import Reptile
+class Snake(Reptile):
+    def __init__(self, name):
+        super().__init__(name)
---
+++ Inheritance/02_Zoo/test.py
@@ -0,0 +1,15 @@
+from project.animal import Animal
+from project.reptile import Reptile
+from project.mammal import Mammal
+from project.lizard import Lizard
+
+
+
+mammal = Mammal("Stella")
+print(mammal.__class__.__bases__[0].__name__)
+print(mammal.name)
+print(mammal._Animal__name)
+lizard = Lizard("John")
+print(lizard.__class__.__bases__[0].__name__)
+print(lizard.name)
+print(lizard._Animal__name)
