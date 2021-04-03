f42122cd11f3d622fc2e321be482f80d8e5d2841
Olga Yordanova <olga.yordanova.oy@gmail.com>
2021-04-03 22:47:46 +0300

    Delete Exam_16_Aug_2020/project/hardware directory

--- Exam_16_Aug_2020/project/hardware/power_hardware.py
+++
@@ -1,6 +0,0 @@
-from project.hardware.hardware import Hardware
-
-class PowerHardware (Hardware):
-    def __init__(self,  name, capacity_s, memory_s):
-        super ().__init__ ( name , type = "Power" ,capacity= int(0.25*capacity_s), memory = int(1.75*memory_s))
-
